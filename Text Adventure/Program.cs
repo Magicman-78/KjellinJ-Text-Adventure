14b369bb2c2aeed053355dac3df340cb9600eea7
Josiah K. <1721887@mukilteo.wednet.edu>
2024-10-04 12:18:31 -0700

    Added notes from the Console I/O video

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            // string insert name here = "whatever you want it to print"; will print whatever message you want
+            // int insert name here = #; will print whatever number you want
+            // bool insert name here = true; will tell you whatever you want is true or false
+            // (string + int + bool); will smoosh all the programmed messages together (string concatenation) 
+            // Console.Foregroundcolor = ConsoleColor.Red; above your message will change the color of the text 
+            // Console.Beep(); will make a beep sound of whatever frequency you type and for however many milliseconds
+            // Console.ReadKey; will make the console wait until you press a button to read the next message
+            // string myMessage = Console.ReadLine(); and Console.Writeline(myMessage); will print back whatever you assign (type) to be myMessage
+
+
+
+
             Console.WriteLine("Hello, World!");
+
+            
         }
     }
 }
